973d4ea52f61acdd3df968ed28c50111ccfc5d5a
sleeter <lfmonsterfeed@gmail.com>
2024-08-19 23:09:06 +0300

    change field of flat

--- internal/model/entity/flat.go
+++ internal/model/entity/flat.go
@@ -1,12 +1,12 @@
 package entity
 
 type Flat struct {
-	Id       int32 `db:"id"`
-	House_id int32 `db:"house_id"`
-	Number   int32 `db:"number"`
-	Price    int32 `db:"price"`
-	Rooms    int32 `db:"rooms"`
-	Status   int32 `db:"status"`
+	Id      int32  `db:"id"`
+	HouseId int32  `db:"house_id"`
+	Number  int32  `db:"number"`
+	Price   int32  `db:"price"`
+	Rooms   int32  `db:"rooms"`
+	Status  string `db:"status"`
 }
 
 const (
