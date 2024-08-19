dcf246bf0698a83aa5f6679371b1f1f5ebbf0293
sleeter <lfmonsterfeed@gmail.com>
2024-08-19 23:09:07 +0300

    add interface for cache

--- internal/repository/deps.go
+++ internal/repository/deps.go
@@ -3,6 +3,7 @@
 package repository
 
 import (
+	"backend-bootcamp-assignment-2024/internal/model/entity"
 	"context"
 	sq "github.com/Masterminds/squirrel"
 	"github.com/jackc/pgx/v4"
@@ -10,4 +11,9 @@ import (
 
 type QueryManager interface {
 	QuerySq(ctx context.Context, query sq.Sqlizer) (pgx.Rows, error)
+}
+type CacheForFlat interface {
+	Add(int32, []entity.Flat) bool
+	Get(int32) ([]entity.Flat, bool)
+	Contains(int32) bool
 }
