0c6538239b75bd10a3e76a540e850421e20ade40
sleeter <lfmonsterfeed@gmail.com>
2024-08-19 23:09:06 +0300

    change fields of migration

--- migrations/1_create_tables.up.sql
+++ migrations/1_create_tables.up.sql
@@ -1,14 +1,14 @@
-CREATE TABLE IF NOT EXISTS house
+CREATE TABLE IF NOT EXISTS houses
 (
   id serial primary key,
   address text not null,
   year int not null check (year >= 0),
   developer text,
-  creation_date timestamp not null default current_timestamp,
-  updated_date timestamp not null default current_timestamp
+  created_at timestamp not null default current_timestamp,
+  update_at timestamp not null default current_timestamp
 );
 
-CREATE TABLE IF NOT EXISTS flat
+CREATE TABLE IF NOT EXISTS flats
 (
   id serial primary key,
   house_id int references house(id),
@@ -23,6 +23,5 @@ CREATE TABLE IF NOT EXISTS users
   user_id uuid primary key,
   email text not null,
   password text not null,
-  user_type varchar(15) not null,
-  dummy bool not null
+  user_type varchar(15) not null
 );
