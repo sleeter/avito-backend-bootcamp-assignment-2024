fe80efc10f6e5065dad788bd6c395b04c1f52eec
sleeter <lfmonsterfeed@gmail.com>
2024-08-19 23:09:06 +0300

    add validation to request fields

--- internal/http-server/handler/flat_handler.go
+++ internal/http-server/handler/flat_handler.go
@@ -2,9 +2,12 @@ package handler
 
 import (
 	"backend-bootcamp-assignment-2024/internal/model/dto/request"
+	"backend-bootcamp-assignment-2024/internal/model/entity"
 	"backend-bootcamp-assignment-2024/internal/service"
+	"errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"slices"
 )
 
 func CreateFlat(ctx *gin.Context, service *service.Service) error {
@@ -13,12 +16,29 @@ func CreateFlat(ctx *gin.Context, service *service.Service) error {
 		ctx.Status(http.StatusBadRequest)
 		return nil
 	}
-	//TODO: validate fields
+	err := validateCreateFlatFields(req)
+	if err != nil {
+		ctx.Status(http.StatusBadRequest)
+		return nil
+	}
 	resp, err := service.FlatService.AddFlat(ctx, req)
 	if err != nil {
 		return err
 	}
 	ctx.JSON(http.StatusOK, resp)
+	return nil
+}
+
+func validateCreateFlatFields(req request.CreateFlat) error {
+	if req.HouseId <= 0 {
+		return errors.New("house id must greater than 0")
+	}
+	if req.Price < 0 {
+		return errors.New("price must greater than 0")
+	}
+	if req.Rooms != nil && *req.Rooms <= 0 {
+		return errors.New("rooms must greater or equal to 0")
+	}
 	return nil
 }
 
@@ -28,11 +48,25 @@ func UpdateFlat(ctx *gin.Context, service *service.Service) error {
 		ctx.Status(http.StatusBadRequest)
 		return nil
 	}
-	//TODO: validate fields
+	err := validateUpdateFlatFields(req)
+	if err != nil {
+		ctx.Status(http.StatusBadRequest)
+		return nil
+	}
 	resp, err := service.FlatService.UpdateFlat(ctx, req)
 	if err != nil {
 		return err
 	}
 	ctx.JSON(http.StatusOK, resp)
+	return nil
+}
+
+func validateUpdateFlatFields(req request.UpdateFlat) error {
+	if req.Id <= 0 {
+		return errors.New("id must greater than 0")
+	}
+	if len(req.Status) != 0 && !slices.Contains(entity.FlatStatuses, req.Status) {
+		return errors.New("status must be one of enum values")
+	}
 	return nil
 }
--- internal/http-server/handler/house_handler.go
+++ internal/http-server/handler/house_handler.go
@@ -3,27 +3,38 @@ package handler
 import (
 	"backend-bootcamp-assignment-2024/internal/model/dto/request"
 	"backend-bootcamp-assignment-2024/internal/service"
+	"errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"strconv"
 )
 
 func GetHouse(ctx *gin.Context, service *service.Service) error {
-	HouseId, err := strconv.ParseInt(ctx.Param("id"), 10, 0)
-	//TODO: add middleware and param to ctx, get isModerator from ctx
-	isModerator := false
-	//TODO: validate fields
+	houseId, err := strconv.ParseInt(ctx.Param("id"), 10, 0)
 	if err != nil {
 		ctx.Status(http.StatusBadRequest)
 		return nil
 	}
-	resp, err := service.FlatService.GetFlats(ctx, int32(HouseId), isModerator)
+	//TODO: add middleware and param to ctx, get isModerator from ctx
+	isModerator := false
+	err = validateGetHouseFields(houseId)
+	if err != nil {
+		ctx.Status(http.StatusBadRequest)
+		return nil
+	}
+	resp, err := service.FlatService.GetFlats(ctx, int32(houseId), isModerator)
 	if err != nil {
 		return err
 	}
 	ctx.JSON(http.StatusOK, gin.H{
 		"flats": resp,
 	})
+	return nil
+}
+func validateGetHouseFields(houseId int64) error {
+	if houseId <= 0 {
+		return errors.New("house id must be greater than 0")
+	}
 	return nil
 }
 
@@ -33,12 +44,28 @@ func CreateHouse(ctx *gin.Context, service *service.Service) error {
 		ctx.Status(http.StatusBadRequest)
 		return nil
 	}
-	//TODO: validate fields
+	err := validateCreateHouseField(req)
+	if err != nil {
+		ctx.Status(http.StatusBadRequest)
+		return nil
+	}
 	resp, err := service.HouseService.CreateHouse(ctx, req)
 	if err != nil {
 		return err
 	}
 	ctx.JSON(http.StatusOK, resp)
+	return nil
+}
+func validateCreateHouseField(req request.House) error {
+	if len(req.Address) == 0 {
+		return errors.New("address must not be empty")
+	}
+	if req.Year < 0 {
+		return errors.New("year must not be less than 0")
+	}
+	if req.Developer != nil && len(*req.Developer) == 0 {
+		return errors.New("developer must not be empty")
+	}
 	return nil
 }
 
