dea0a39622a53845e1423ca9ab71e9fd00b7b78e
sleeter <lfmonsterfeed@gmail.com>
2024-08-19 23:09:07 +0300

    binding

--- internal/model/dto/request/request.go
+++ internal/model/dto/request/request.go
@@ -14,23 +14,23 @@ type Login struct {
 }
 
 type CreateFlat struct {
-	HouseId int32  `json:"house_id" binding:"required"`
-	Price   int32  `json:"price" binding:"required"`
+	HouseId int32  `json:"house_id" binding:"required,gte=1"`
+	Price   int32  `json:"price" binding:"required,gte=0"`
 	Rooms   *int32 `json:"rooms"`
 	Status  string `json:"status"`
 }
 type UpdateFlat struct {
-	Id     int32  `json:"id" binding:"required"`
+	Id     int32  `json:"id" binding:"required,gte=1"`
 	Status string `json:"status"`
 }
 
 type House struct {
 	Address   string  `json:"address" binding:"required"`
-	Year      int32   `json:"year" binding:"required"`
-	Developer *string `json:"developer"`
+	Year      int32   `json:"year" binding:"required,gte=0"`
+	Developer *string `json:"developer" binding:""`
 }
 
 type Subscriber struct {
 	HouseId int32  `json:"house_id"`
-	Email   string `json:"email" binding:"required"`
+	Email   string `json:"email" binding:"required,email"`
 }
