f42ee9554b2cfdde07e01e90c4d4898152026229
sleeter <lfmonsterfeed@gmail.com>
2024-08-19 23:09:06 +0300

    fix repo field

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -3,7 +3,7 @@ package service
 type UserRepository interface{}
 
 type UserService struct {
-	Repository         HouseRepository
+	Repository         UserRepository
 	TransactionManager TransactionManager
 }
 
