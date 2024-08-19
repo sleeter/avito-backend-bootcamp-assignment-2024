1349ea2387a8dbf5ba207bb293df1a776533d45e
sleeter <lfmonsterfeed@gmail.com>
2024-08-19 23:09:06 +0300

    return ptr and add create house method

--- internal/service/house_service.go
+++ internal/service/house_service.go
@@ -1,14 +1,16 @@
 package service
 
 import (
+	"backend-bootcamp-assignment-2024/internal/mapper"
 	"backend-bootcamp-assignment-2024/internal/model/dto/request"
+	"backend-bootcamp-assignment-2024/internal/model/dto/response"
 	"backend-bootcamp-assignment-2024/internal/model/entity"
 	"context"
 	"time"
 )
 
 type HouseRepository interface {
-	CreateHouse(ctx context.Context, house request.House) (entity.House, error)
+	CreateHouse(ctx context.Context, house request.House) (*entity.House, error)
 	UpdateHouse(ctx context.Context, houseId int32, updateTime time.Time) error
 }
 
@@ -22,4 +24,12 @@ func NewHouseService(r HouseRepository, manager TransactionManager) HouseService
 		Repository:         r,
 		TransactionManager: manager,
 	}
+}
+
+func (s *HouseService) CreateHouse(ctx context.Context, house request.House) (*response.House, error) {
+	res, err := s.Repository.CreateHouse(ctx, house)
+	if err != nil {
+		return nil, err
+	}
+	return mapper.HouseEntityToHouseResponse(res), nil
 }
